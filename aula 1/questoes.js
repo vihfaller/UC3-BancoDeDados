bc63b9d1524de96e37d60a4c4206ad83ed9a6e56
vih_faller <vitoriafallerbier@gmail.com>
2024-10-21 14:49:07 -0300

    aula 1

---
+++ aula 1/questoes.js
@@ -0,0 +1,21 @@
+/*
+1) Em que ano o modelo relacional de banco de dados foi desenvolvido?
+        Em 1970-1972
+
+2) Quem foi considerado o 'pai dos bancos de dados'?
+        Tod Codd (nao me recordo como escreve)
+
+3) Explique a diferença de um banco de daods, Sistema Gerenciador de Banco de dados e Sistema Banco de Dados.
+        -Banco de dados: é uma forma de armarmazenarmos todas as informaçoes e dados de algum assunto especifico em sistemas.
+        -Sistema Gerenciador de Banco de Dados: Local onde esta guardado todas as informaçoes e dados do sistema.
+        -Sistema Banco de Dados: sistema de informaçoes de algum assunto com teus atributos.
+
+4) Quais são as principais características de um banco de dados?
+        Qual o seu nível de fato (baixo, medio ou alto nivel), aqueles negocios de conexoes de dados dos atributos e entidades e a forma como é armazenada.
+
+5) Quais são os diferentes níveis de abstração em um banco de daods.
+        Baixo, médio e alto (físico, lógico, conceitual).
+
+6) Liste as vantagens de usar um banco de dados:
+        Podemos acessar qualquer informação de qualquer lugar, ter conhecimento de estimativas em determidado assunto, nao me recordo mais de vantagens.
+*/
