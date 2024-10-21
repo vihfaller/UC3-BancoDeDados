5d83126cc9f4bbe623fa71b7c7115ffe38ca7f94
vih_faller <vitoriafallerbier@gmail.com>
2024-10-21 16:06:36 -0300

    aula1

--- aula 1/questoes.js
+++ aula 1/questoes.js
@@ -3,19 +3,19 @@
         Em 1970-1972
 
 2) Quem foi considerado o 'pai dos bancos de dados'?
-        Tod Codd (nao me recordo como escreve)
+        Ted Edgard Frank Codd
 
 3) Explique a diferença de um banco de daods, Sistema Gerenciador de Banco de dados e Sistema Banco de Dados.
-        -Banco de dados: é uma forma de armarmazenarmos todas as informaçoes e dados de algum assunto especifico em sistemas.
-        -Sistema Gerenciador de Banco de Dados: Local onde esta guardado todas as informaçoes e dados do sistema.
-        -Sistema Banco de Dados: sistema de informaçoes de algum assunto com teus atributos.
+        -SGBD: postgresg/mysql
+        -BD: Tabelas/views
+        -SBD: é oq engloba os outros dois pontos
 
 4) Quais são as principais características de um banco de dados?
-        Qual o seu nível de fato (baixo, medio ou alto nivel), aqueles negocios de conexoes de dados dos atributos e entidades e a forma como é armazenada.
+        Escalabilidade, integridade, segurança
 
 5) Quais são os diferentes níveis de abstração em um banco de daods.
         Baixo, médio e alto (físico, lógico, conceitual).
 
 6) Liste as vantagens de usar um banco de dados:
-        Podemos acessar qualquer informação de qualquer lugar, ter conhecimento de estimativas em determidado assunto, nao me recordo mais de vantagens.
+        Segurança, tira conflito de redundancia de daods, restauraçao do banco
 */
