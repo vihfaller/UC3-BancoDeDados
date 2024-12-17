be265bd08509c23bea8fc1bc988e01638297131c
vih_faller <vitoriafallerbier@gmail.com>
2024-12-17 16:16:52 -0300

    Murder Mistery

--- murder mistery/trabalho.sql
+++ murder mistery/trabalho.sql
@@ -18,3 +18,22 @@ INSERT INTO solution VALUES (1, 'Jeremy Bowers');
         
         SELECT value FROM solution;
 -- Parabéns, você encontrou o assassino! Mas espere, tem mais... Se você acha que está pronto para um desafio, tente consultar a transcrição da entrevista do assassino para encontrar o verdadeiro vilão por trás deste crime. Se você se sente especialmente confiante em suas habilidades de SQL, tente completar esta etapa final com no máximo 2 consultas. Use esta mesma instrução INSERT com seu novo suspeito para verificar sua resposta.
+-- ID JEREMY 67318
+
+select * from interview where person_id = 67318
+-- Fui contratado por uma mulher com muito dinheiro. Não sei o nome dela, mas sei que ela tem cerca de 5'5" (65") ou 5'7" (67"). Ela tem cabelo ruivo e dirige um Tesla Model S. Sei que ela foi ao SQL Symphony Concert 3 vezes em dezembro de 2017. 
+
+select * from drivers_license where height = 65 and hair_color = 'red' and car_model = 'Tesla Model S'
+-- 202298	68	66	green	red	female	500123	Tesla	Model S
+-- 291182	65	66	blue	red	female	08CM64	Tesla	Model S
+-- 918773	48	65	black	red	female	917UU3	Tesla	Model S
+ 
+select * from person where license_id = 202298 --Miranda Priestly 99716
+select * from person where license_id = 291182 --Regina George 90700
+select * from person where license_id = 918773 -- Red Korb 78881
+
+select * from facebook_event_checkin where person_id = 99716
+--  99716	1143	SQL Symphony Concert	20171206
+--  99716	1143	SQL Symphony Concert	20171212
+--  99716	1143	SQL Symphony Concert	20171229
+
